de5550f92b608cf0b5c4b74443ed23f5a84805c7
Christian Ramelow <info@codekandis.net>
2023-07-23 01:08:50 +0200

    Adds a URI builder builder.

---
+++ src/Environment/Http/UriBuilders/UriBuilderBuilder.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CheckList\Environment\Http\UriBuilders;
+
+use CodeKandis\Tiphy\Http\UriBuilders\AbstractUriBuilderBuilder;
+
+/**
+ * Represents a URI builder builder.
+ * @package codekandis/checklist
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class UriBuilderBuilder extends AbstractUriBuilderBuilder implements UriBuilderBuilderInterface
+{
+}
---
+++ src/Environment/Http/UriBuilders/UriBuilderBuilderInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CheckList\Environment\Http\UriBuilders;
+
+/**
+ * Represents the interface of any URI builder builder.
+ * @package codekandis/checklist
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface UriBuilderBuilderInterface
+{
+}
