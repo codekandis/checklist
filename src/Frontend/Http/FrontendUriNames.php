017c5f83df6bf373f2b133b4639b543e56e3031b
Christian Ramelow <info@codekandis.net>
2023-07-25 22:26:52 +0200

    Adds an enumeration of frontend URI names.

---
+++ src/Frontend/Http/FrontendUriNames.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CheckList\Frontend\Http;
+
+/**
+ * Represents an enumeration of frontend URI names.
+ * @package codekandis/checklist
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class FrontendUriNames
+{
+}
