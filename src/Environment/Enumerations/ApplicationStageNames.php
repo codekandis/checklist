6ef678577a987605b97b0373a950e26af1304b80
Christian Ramelow <info@codekandis.net>
2023-07-25 22:33:53 +0200

    Adds an enumeration of application stage names.

---
+++ src/Environment/Enumerations/ApplicationStageNames.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CheckList\Environment\Enumerations;
+
+/**
+ * Represents an enumeration of application stage names.
+ * @package codekandis/checklist
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class ApplicationStageNames
+{
+}
