3a9e538734f7c3e88f9ca492d1004b48e75d4f49
Christian Ramelow <info@codekandis.net>
2023-07-27 00:06:13 +0200

    Adds the JavaScript `ApiUriNames`.

---
+++ public/assets/javascript/Net/ApiUriNames.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { AbstractStatic } from '../../libraries/jotunheim/Types/AbstractStatic.js';
+
+/**
+ * Represents an enumeration of API URI names.
+ */
+export class ApiUriNames extends AbstractStatic
+{
+}
