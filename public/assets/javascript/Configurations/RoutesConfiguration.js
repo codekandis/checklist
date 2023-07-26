449d1882381454592c79de6c3018ceee345d603a
Christian Ramelow <info@codekandis.net>
2023-07-27 01:24:55 +0200

    Adds the JavaScript `RoutesConfiguration`.

---
+++ public/assets/javascript/Configurations/RoutesConfiguration.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import { AbstractRoutesConfiguration } from '../../libraries/jotunheim/Configurations/AbstractRoutesConfiguration.js';
+
+/**
+ * Represents a routes configuration.
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+export class RoutesConfiguration extends AbstractRoutesConfiguration
+{
+	/**
+	 * @inheritdoc
+	 */
+	__routes = {};
+}
