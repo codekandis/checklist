dc4019c521b15e1acb79ba882dda9eff1fdaf064
Christian Ramelow <info@codekandis.net>
2023-07-27 01:09:22 +0200

    Adds the JavaScript `FrontendUriNames`.

---
+++ public/assets/javascript/Net/FrontendUriNames.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { AbstractStatic } from '../../libraries/jotunheim/Types/AbstractStatic.js';
+
+/**
+ * Represents an enumeration of frontend URI names.
+ */
+export class FrontendUriNames extends AbstractStatic
+{
+}
