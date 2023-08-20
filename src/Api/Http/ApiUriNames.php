29b155b378f707a8dfb0013d40e0ba57c06b4433
Christian Ramelow <info@codekandis.net>
2023-08-20 17:52:52 +0200

    Adds an enumeration of API URI names.

---
+++ src/Api/Http/ApiUriNames.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\CheckList\Api\Http;
+
+/**
+ * Represents an enumeration of API URI names.
+ * @package codekandis/checklist
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+abstract class ApiUriNames
+{
+}
